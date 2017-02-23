9735c222da2457c17e73c29bcfef6fad17e7fcc3
1o01 <wengwangjay@126.com>
2017-02-24 00:12:52 +0800

    测试部分编写完成，目前只需要提供对应的正则表达式即可

--- src/browser-test/browser-test-1.0.js
+++ src/browser-test/browser-test-1.0.js
@@ -13,11 +13,10 @@
          * @type {Object}
          */
         this.uaInfo = {
-            browser: {},
-            os_name: '', // 操作系统名称
-            os_bit: '', // 操作系统位数
-            os_ver: '', // 操作系统版本
-            client: '' // 客户端类型
+            browser: {}, // 浏览器名称及版本号
+            os: {ver:''}, // 操作系统及版本号
+            client: {}, // 客户端类型
+            enigne: {} // 内核类型及版本号
         };
 
         /**
@@ -26,47 +25,71 @@
          */
         this.regexp = {
             browser: {
-                IE: /MSIE\ (\d+)/gi, // IE 浏览器
-                IE11: /rv:11\.0/gi, // IE11 浏览器
-                Edge: /Edge(\/\d+)/gi, // IE Edge 浏览器
-                Chrome: /Chrome\/(\d+)/gi, // Chrome 浏览器
-                FireFox: /Firefox\/(\d+)/gi, // 火狐浏览器
-                Safari: /Safari\/(\d+)/gi, // Safari 浏览器
-                Opera: /OPR\/(\d+)/gi, // Opera 浏览器
-                Wecat: /MicroMessenger\/(\d+)/gi // 微信内置浏览器
+                IE: /MSIE\ (\d+)/ig, // IE 浏览器
+                IE11: /rv:11\.0/ig, // IE11 浏览器
+                Edge: /Edge(\/\d+)/ig, // IE Edge 浏览器
+                Chrome: /Chrome\/(\d+)/ig, // Chrome 浏览器
+                FireFox: /Firefox\/(\d+)/ig, // 火狐浏览器
+                Safari: /Safari\/(\d+)/ig, // Safari 浏览器
+                Opera: /OPR\/(\d+)/ig, // Opera 浏览器
+                Wecat: /MicroMessenger\/(\d+)/ig // 微信内置浏览器
             },
             os: {
-                Window: /Windows NT (\d+\.\d+); (WOW\d*){0,1}/gi, // Windows 操作系统
-                // osx: '', // 苹果桌面操作系统
-                IOS: /iP.*; .* OS (\d+_\d)/i, // 苹果移动设备操作系统
-                Andriod: /Linux; U;{0,1} Android (\d+\.\d+)/gi, // 安卓
-                // linux: '' // Linux
+                Window: /Windows\ NT\ (\d+\.\d+);\ (WOW\d*){0,1}/ig, // Windows 操作系统
+                MacOS: /Mac\ OS\ X\ (\d+_\d+_\d+)/, // 苹果桌面操作系统
+                IOS: /iP.*;\ .*\ OS\ (\d+_\d)/ig, // 苹果移动设备操作系统
+                Andriod: /Android\ (\d+\.\d+)/ig, // 安卓
+                linux: /Linux/ // Linux
             },
             client: {
-                // pc: '', // PC设备
-                iPhone: /iPhone/gi, // 苹果手机
-                iPad: /iPad/gi, // 苹果平板设备
-                Andriod: /Android/gi // 安卓设备
+                PC: /Windows/, // PC设备
+                iPhone: /iPhone/ig, // 苹果手机
+                iPad: /iPad/ig, // 苹果平板设备
+                Andriod: /Android/ig // 安卓设备
             },
-            engine: {
-                WebKit:'',
-                Trident: '',
-                Gecko: '',
-                AppleWebKit: '',
+            enigne: {
+                WebKit:/WebKit\/(\d+)/ig,
+                Trident: /Trident\/(\d+)/ig,
+                Gecko: /Gecko\/(\d+)/ig,
+                AppleWebKit: /AppleWebKit\/(\d+)/ig
             }
 
         };
     }
 
     BrowserTest.prototype.start = function () { // 浏览器检测启动方法
-        // this.testOthers();
-        this.testBrowser();
-        this.testOS();
-
+        this.testFull();
         window.UAInfo = this.uaInfo;
     };
 
+    /**
+     * [testBrowser 测试所有信息]
+     */
+    BrowserTest.prototype.testFull = function(){
+        var ua = this.ua;
+        var regexps = this.regexp;
 
+        for(var re in regexps){
+        	var _re = regexps[re];
+            for(var item in _re){
+            	var result = _re[item].exec(ua);
+            	if(result){
+            		if(re === 'os'){
+            			this.uaInfo[re][item] = item;
+            			this.uaInfo[re]['ver'] = result[1];
+            		}else{
+            			this.uaInfo[re][item] = result[1];
+            		}
+            	}else{
+            		this.uaInfo[re][item] = null;
+            	}
+            }
+        }
+    };
+
+    /**
+     * [testBrowser 测试浏览器信息]
+     */
     BrowserTest.prototype.testBrowser = function(){
         var ua = this.ua;
         var reBrowser = this.regexp.browser;
@@ -83,7 +106,6 @@
 
     /**
      * [testOS 测试操作系统信息]
-     * @return {[type]} [description]
      */
     BrowserTest.prototype.testOS = function(){
         var ua = this.ua;
@@ -91,117 +113,50 @@
 
         for(var os in reOS){
             var result = reOS[os].exec(ua);
-            console.log(result)
             if(result){
-                this.uaInfo.os_name = os;
-                this.uaInfo.os_ver = result[1];
-                this.uaInfo.os_bit = result[2] ? result[2] : null;
+                this.uaInfo.os[os] = os;
+                this.uaInfo.os[ver] = result[1];
+            }else{
+                this.uaInfo.os[os] = null;
             }
         }
     };
 
     /**
-     * [测试IE浏览器]
-     * @return {[Object Boolean]}         [返回浏览器的版本信息]
+     * [textClient 测试客户端类型]
      */
-    BrowserTest.prototype.testIE = function () {
+    BrowserTest.prototype.textClient = function(){
         var ua = this.ua;
-        var result;
+        var reClient = this.regexp.client;
 
-        var reIE = this.regexp.browser.ie;
-        var reIEEdge = this.regexp.browser.edge;
-        var re11 = this.regexp.browser.ie11;
-        var ieVer;
-
-        var ieType = ua.match(reIE);
-
-        if (ieType !== null) {
-            ieVer = ieType[1];
-            result = {type: 'ie', ver: ieVer};
-        } else if (reIEEdge.test(ua)) {
-            ieVer = 'edge';
-            result = {type: 'ie', ver: ieVer};
-        } else if (re11.test(ua)) {
-            ieVer = '11';
-            result = {type: 'ie', ver: ieVer};
-        } else {
-            result = {type: false, ver: false};
+        for(var client in reClient){
+        	var result = reClient[client].exec(ua);
+        	if(result){
+        		this.UAInfo.client[client] = true;
+        	}else{
+        		this.UAInfo.client[client] = null;
+        	}
         }
-
-        return result;
     };
 
     /**
-     * [测试Chrome浏览器]
-     * @return {[Object Boolean]}         [返回浏览器的版本信息]
+     * [textEnigne 测试浏览器内核类型]
      */
-    BrowserTest.prototype.testChrome = function () {
-        var ua = this.ua;
-        var result;
+    BrowserTest.prototype.textEnigne = function(){
+    	var ua = this.ua;
+        var reEnigne = this.regexp.enigne;
 
-        var reChrome = this.regexp.browser.chrome;
-        var chromeVer;
-
-        var chromeType = ua.match(reChrome);
-
-
-        if (chromeType !== null) {
-            chromeVer = chromeType[1];
-            result = {type: 'chrome', ver: chromeVer};
-        } else {
-            result = {type: false, ver: false};
-        }
-        return result;
-
-    };
-
-    /**
-     * [测试FireFox浏览器]
-     * @return {[Object Boolean]}         [返回浏览器的版本信息]
-     */
-    BrowserTest.prototype.testFireFox = function () {
-        var ua = this.ua;
-        var result;
-
-        var reFirefox = this.regexp.browser.firefox;
-        var firefoxVer;
-
-        var firefoxType = ua.match(reFirefox);
-
-
-        if (firefoxType !== null) {
-            firefoxVer = firefoxType[1];
-            result = {type: 'firefox', ver: firefoxVer};
-        } else {
-            result = {type: false, ver: false};
-        }
-
-        return result;
-    };
-
-    /**
-     * [测试所有浏览器]
-     * @return {[Object]}         [给回调函数传递浏览器的信息]
-     */
-    BrowserTest.prototype.testOthers = function () {
-
-        var ieResult = this.testIE();
-        var chromeResult = this.testChrome();
-        var firefoxResult = this.testFireFox();
-        if (ieResult.type) {
-            window.BrowserInfo = ieResult;
-        } else if (chromeResult.type) {
-            window.BrowserInfo = chromeResult;
-        } else if (firefoxResult.type) {
-            window.BrowserInfo = firefoxResult;
-        } else {
-            window.BrowserInfo = {type: false, ver: false};
-            console.error('不是你的错，就是我的错，还没研究出如何判断你的浏览器');
+        for(var enigne in reEnigne){
+        	var result = reEnigne[enigne].exec(ua);
+        	if(result){
+        		this.UAInfo.enigne[enigne] = result[1];
+        	}else{
+        		this.UAInfo.enigne[enigne] = null;
+        	}
         }
     };
 
-    var _BrowserTest = new BrowserTest();
-
-    _BrowserTest.start();
+   	var _BrowserTest = new BrowserTest();
+   	_BrowserTest.start();
 
 })();
