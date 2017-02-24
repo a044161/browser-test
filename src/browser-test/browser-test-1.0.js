ad839171fdf4f544b1eb47ba84dbe291578e6c1a
翁旺 <311519@>
2017-02-24 16:13:23 +0800

    1.0 版本开发完成，将ie11的判断添加到ie中 - readme 未编写

--- src/browser-test/browser-test-1.0.js
+++ src/browser-test/browser-test-1.0.js
@@ -7,14 +7,13 @@
     'use strict';
     function BrowserTest() {
         this.ua = navigator.userAgent;
-
         /**
          * [uaInfo 浏览器信息]
          * @type {Object}
          */
         this.uaInfo = {
             browser: {}, // 浏览器名称及版本号
-            os: {ver:''}, // 操作系统及版本号
+            os: {bit: ''}, // 操作系统及版本号
             client: {}, // 客户端类型
             enigne: {} // 内核类型及版本号
         };
@@ -25,8 +24,7 @@
          */
         this.regexp = {
             browser: {
-                IE: /MSIE\ (\d+)/ig, // IE 浏览器
-                IE11: /rv:11\.0/ig, // IE11 浏览器
+                IE: /MSIE\ (\d+)|rv:(11)\.0/ig, // IE 浏览器
                 Edge: /Edge\/(\d+)/ig, // IE Edge 浏览器
                 Chrome: /Chrome\/(\d+)/ig, // Chrome 浏览器
                 FireFox: /Firefox\/(\d+)/ig, // 火狐浏览器
@@ -35,10 +33,10 @@
                 Wecat: /MicroMessenger\/(\d+)/ig // 微信内置浏览器
             },
             os: {
-                Windows: /Windows\ NT\ (\d+\.\d+);\ (WOW\d*){0,1}/ig, // Windows 操作系统
+                Windows: /Windows\ NT\ (\d+\.\d+);\ WOW(\d*){0,1}/ig, // Windows 操作系统
                 MacOS: /Mac\ OS\ X\ (\d{0,9}_?\d{0,9}_?\d{0,9})/, // 苹果桌面操作系统
                 IOS: /iP.*;.*\ OS\ (\d+_?\d+_?\d{0,9})/ig, // 苹果移动设备操作系统
-                Andriod: /Android\ (\d{0,9}\.?\d{0,9}\.?\d{0,9})/ig, // 安卓
+                Android: /Android\ (\d{0,9}\.?\d{0,9}\.?\d{0,9})/ig, // 安卓
                 Linux: /Linux/ig, // Linux
                 BlackBerry: /BB(\d+)/ig, // 黑莓
                 Tablet: /Tablet\ OS\ (\d{0,9}\.?\d{0,9}\.?\d{0,9})/ig, // 黑莓
@@ -48,8 +46,8 @@
                 PC: /Windows|Mac\ OS\ X\ /ig, // PC设备
                 iPhone: /iPhone/ig, // 苹果手机
                 iPad: /iPad/ig, // 苹果平板设备
-                Andriod: /Android/ig, // 安卓设备
-                Moblie: /iPhone|Android|BB\d+|PlayBook/ig,
+                Android: /Android/ig, // 安卓设备
+                Mobile: /iPhone|Android|BB\d+|PlayBook|Mobile/ig,
                 Pad: /iPad/ig
             },
             enigne: {
@@ -81,8 +79,17 @@
             	if(result){
             		if(re === 'client'){
             			this.uaInfo[re][item] = item;
-            		}else{
-            			this.uaInfo[re][item] = result[1] ? result[1] : item;
+            		}else if(re === 'os' && result[2]){ // 获取操作系统的位数
+                        this.uaInfo[re][item] = result[1]; // 操作系统版本
+                        this.uaInfo[re].bit = result[2]; // 操作系统位数
+                    }else{
+                        if(result[1]){
+                            this.uaInfo[re][item] = result[1];
+                        }else if(result[2]){
+                            this.uaInfo[re][item] = result[2];
+                        }else{
+                            this.uaInfo[re][item] = item;
+                        }
             		}
             	}else{
             		this.uaInfo[re][item] = null;
